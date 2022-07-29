ced99217fd9ce17f416a931760c50524df2d58dd
kaustubh thakur <93928807+kaustubhthakur@users.noreply.github.com>
2022-07-29 19:33:23 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+// hardhat.config.js
+
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+//const GOERLI_URL = process.env.GOERLI_URL;
+//Const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: YOUR_GOERLI_URL,
+      accounts: [YOUR_PRIVATE_KEY]
+    }
+  }
+};
